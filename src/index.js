9fd27fc1920762d9bdb529e3861ba2d9028e27fb
luckyshroom <artemletter@gmail.com>
2019-01-04 01:08:41 +0700

    Dev environment

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Logo} from "./svg/Logo";
+
+ReactDOM.render(
+    <div className="wrapper">
+        <nav className="navbar has-shadow is-fixed-top-desktop">
+            <div className="container">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="https://depa.io"><Logo width="80" height="40"/></a>
+                </div>
+            </div>
+        </nav>
+        <main className="main">
+            <div className="content">
+                <div className="column"><text>Hello!</text></div>
+            </div>
+        </main>
+    </div>,
+    document.getElementById('app')
+);
