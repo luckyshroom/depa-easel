2b3a369e3edec41496e63b68591bb1eb14d2e87a
luckyshroom <artemletter@gmail.com>
2020-03-17 01:08:25 +0700

    twitter support added

--- src/components/blocks/TwitterBlock.js
+++ src/components/blocks/TwitterBlock.js
@@ -3,6 +3,6 @@ import {TwitterTweetEmbed} from "react-twitter-embed";
 
 export const TwitterBlock = ({data}) => (
     <div className="twitter-embed">
-        <TwitterTweetEmbed tweetId={data.tweetId}/>
+        <TwitterTweetEmbed tweetId={data.tweetId} onLoad={e => e.style = null}/>
     </div>
 );
